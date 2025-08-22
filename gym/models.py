ba1629a68425be53ba8a9d00597f10b216795448
Cleomar-Junior <cl_jr@outlook.com>
2025-08-21 22:11:38 -0300

    Criação do modelo de Treino

--- gym/models.py
+++ gym/models.py
@@ -27,3 +27,13 @@ class Mensalidade(models.Model):
     def __str__(self):
         return f"{self.aluno.nome} - Pago em {self.data_pagamento}"
     
+
+class Treino(models.Model):
+    aluno = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="treinos")
+    personal = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True, blank=True, related_name="treinos_orientados")
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nome} ({self.aluno.nome})"
