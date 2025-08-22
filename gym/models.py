7d05b520778d7ea53e9ffa6f77d2d49d1c9d7282
Cleomar-Junior <cl_jr@outlook.com>
2025-08-21 22:10:26 -0300

    Criação do modelo de mensalidade

--- gym/models.py
+++ gym/models.py
@@ -11,9 +11,19 @@ class Usuario(models.Model):
     nome = models.CharField(max_length=100)
     data_nascimento = models.DateField(null=True, blank=True)
     sexo = models.CharField(max_length=1, choices=SEXO_CHOICES, null=True, blank=True)
-    is_personal = models.BooleanField(default=False)  # True = personal, False = aluno
+    is_personal = models.BooleanField(default=False)  # True = personal
     data_inscricao = models.DateField(auto_now_add=True)
 
     def __str__(self):
         tipo = "Personal" if self.is_personal else "Aluno"
         return f"{self.nome} ({tipo})"
+    
+class Mensalidade(models.Model):
+    aluno = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="mensalidades")
+    data_pagamento = models.DateField()
+    valor = models.DecimalField(max_digits=8, decimal_places=2)
+    validade = models.DateField()  # Validade da mensalidade
+
+    def __str__(self):
+        return f"{self.aluno.nome} - Pago em {self.data_pagamento}"
+    
