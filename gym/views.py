04a5f5099bd892fc9242a5262c65ca392dafa5e3
Cleomar-Junior <cl_jr@outlook.com>
2025-09-03 21:42:58 -0300

    Alteração nas views, crud de usuário

--- gym/views.py
+++ gym/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from .models import Usuario, Mensalidade, Treino, Exercicio
+from .serializers import (
+    UsuarioSerializer, 
+    MensalidadeSerializer, 
+    TreinoSerializer, 
+    TreinoCreateSerializer,
+    ExercicioSerializer
+)
